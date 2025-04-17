c2dd97d5b6e1518cbe693aef08176c8734864a49
kaktus <bin9825aaa@gmail.com>
2025-04-17 17:27:33 +0900

    초기 커밋.

---
+++ GameDemo/GameDemo.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include <windows.h>
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow)
+{
+   /* DemoGameApp app;
+
+    app.Initialize();
+
+    app.Run();
+    app.Shutdown();*/
+
+    return (int)1;
+}
